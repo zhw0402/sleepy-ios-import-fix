bb1a5bce4c1b55c6a025156aed96815f2b604267
zhw0402 <142647584+zhw0402@users.noreply.github.com>
2026-09-16 15:34:14 +0800

    fix(build): CFStringEncodings 需取 .rawValue

--- Sleepy/util/TextFileReader.swift
+++ Sleepy/util/TextFileReader.swift
@@ -92,7 +92,7 @@ enum TextFileReader {
         for cfEnc in [CFStringEncodings.GB_18030_2000,
                       CFStringEncodings.GB_2312_80,
                       CFStringEncodings.big5] {
-            let enc = nsEncoding(cfEnc)
+            let enc = nsEncoding(cfEnc.rawValue)
             if let s = String(data: data, encoding: enc) { return s }
         }
 
